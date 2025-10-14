b7799ddc6074d0e396a825967cd3c141103a011c
Esteban Mongui <esteband.itorres@gmail.com>
2025-10-14 10:22:58 -0600

    refactor: implement the new interfaces and concrete class for TXT financial statements

--- statements_processor.py
+++ statements_processor.py
@@ -1,22 +1,23 @@
 # Importar librerías
-import pandas as pd
 import regex as re
 
 from FileManager import FileManager
+from processor import FinancialStatementsProcessor
+from file import File
 
 # Todo: this should be received as parameter
 date_pattern = r"[0-9]+[A-Za-z]{3}[0-9]+ +([A-Z]+ )+ +\$"
 fm = FileManager()
 
 
-class ExpensesProcessor:
-    def __init__(self, file_path, file_name):
-        self.__file_path = file_path
-        self.__file_name = file_name
-        self.expenses = None
+class TxtExpensesProcessor(FinancialStatementsProcessor):
+    def __init__(self, statements_files: File, data_structure):
+        self.__statements_files = statements_files
+        self.data_structure = data_structure
 
-    def get_header(self, file):
+    def get_header(self, file, data_structure):
         head_line = ""
+        print("data_structure: ", data_structure) # TODO: remove
         for line in file:
             if "Fecha" in line and "Valor a" in line:
                 # FIXME: write code to detect the head of the expenses table
@@ -24,50 +25,57 @@ class ExpensesProcessor:
         return head_line
 
     @property
-    def file_path(self):
-        return self.__file_path
+    def statements_files(self):
+        return self.__statements_files
 
-    @staticmethod
-    def __get_expense_lines(self):
-        @fm.read_file(self.file_path)
-        def get_file(file):
-            expenses = []
-            for line in file:
-                if re.search(date_pattern, line):
-                    expenses.append(line)
-
-            return expenses
-
-        expenses = get_file()
-        return expenses 
-
-    @staticmethod
-    def __parse_expenses(expenses):
+    def clean_movements(self, movements):
         spaces_pattern = r" {2,}"
         enter_pattern = r"\n"
-        expense_records = []
-        for expense in expenses:
-            _expense = re.sub(spaces_pattern, "|", expense)
+        cleaned_movements = []
+        for movement in movements:
+            _expense = re.sub(spaces_pattern, "|", movement)
             _expense = re.sub(enter_pattern, "", _expense)
-            expense_records.append(_expense.split("|"))
+            cleaned_movements.append(_expense.split("|"))
 
-        for record in expense_records:
-            del record[4:]
-            record.pop(0)
+        for movement in cleaned_movements:
+            del movement[4:]
+            movement.pop(0)
 
-        return expense_records
+        return cleaned_movements 
+    
+    def get_movements_by_file(self, statement_file):
+        @fm.read_file(statement_file['path'])
+        def get_movements(file):
+            movements = []
+            for line in file:
+                if re.search(self.data_structure['pattern'], line):
+                    movements.append(line)
+            return movements
 
-    def generate_expenses(self):
-        file_path = self.file_path
-        expense_lines = self.__get_expense_lines(self)
-        expenses = self.__parse_expenses(expense_lines)
+        return get_movements()
+    
+    def get_movements(self):
+        all_movements = []
+        for statement_file in self.statements_files:
+            movements = self.get_movements_by_file(statement_file)
+            all_movements.extend(movements)
+        return all_movements 
 
-        return expenses
+
+    def process(self):
+        movements = self.get_movements()
+        cleaned_movements = self.clean_movements(movements)
+        return cleaned_movements
 
 
 if __name__ == "__main__":
+    data_structure = {
+        "pattern": date_pattern,
+    }
+
     file_path = "./data/julio.txt"
-    file_name = ""
-    processor = ExpensesProcessor(file_path, file_name)
-    expenses = processor.generate_expenses()
-    print('Expenses: ', expenses)
+    file_name = "julio"
+
+    processor = TxtExpensesProcessor([{'type': "txt", 'path': file_path, 'name': file_name}], data_structure)
+    movements = processor.process()
+    print(movements)
